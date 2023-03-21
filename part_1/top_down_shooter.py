e9ef4019f29a9af7e295f0eb71176d07b89421b6
JCode1029 <122577008+JCode1029@users.noreply.github.com>
2023-03-21 15:14:37 +0000

    Create top_down_shooter.py

---
+++ part_1/top_down_shooter.py
@@ -0,0 +1,63 @@
+import pygame
+from sys import exit
+import math
+from settings import *
+
+pygame.init()
+
+# Creating the window
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Top Down Shooter")
+clock = pygame.time.Clock()
+
+# Loads images
+background = pygame.transform.scale(pygame.image.load("background/background.png").convert(), (WIDTH, HEIGHT))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.transform.rotozoom(pygame.image.load("player/0.png").convert_alpha(), 0, PLAYER_SIZE)
+        self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.speed = PLAYER_SPEED
+
+    def user_input(self):
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.velocity_y = -self.speed
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+        if keys[pygame.K_a]:
+            self.velocity_x = -self.speed
+
+        if self.velocity_x != 0 and self.velocity_y != 0: # moving diagonally
+            self.velocity_x /= math.sqrt(2)
+            self.velocity_y /= math.sqrt(2)
+
+    def move(self):
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+
+    def update(self):
+        self.user_input()
+        self.move()
+
+player = Player()
+
+while True:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(background, (0, 0))
+    screen.blit(player.image, player.pos)
+    player.update()
+
+    pygame.display.update()
+    clock.tick(FPS)
