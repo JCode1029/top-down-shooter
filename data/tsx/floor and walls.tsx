1ef8e3eccc08862de9e9e7c51956327dd16f3bd5
JCode1029 <122577008+JCode1029@users.noreply.github.com>
2023-05-10 01:16:58 +0100

    Add files via upload

---
+++ data/tsx/floor and walls.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="floor and walls" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="../../dungeon tileset 2/Tiles-SandstoneDungeons.png" width="288" height="224"/>
+ <tile id="1" probability="0.025"/>
+ <tile id="2" probability="0.975"/>
+</tileset>
---
+++ data/tsx/props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="props" tilewidth="32" tileheight="32" tilecount="99" columns="11">
+ <image source="../../dungeon tileset 2/Tiles-Props-pack.png" width="352" height="288"/>
+</tileset>
